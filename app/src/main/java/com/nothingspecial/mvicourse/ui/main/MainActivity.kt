af8908d4ddefe5aa6233ceb0d5d9a544deb7ad7e
154773 <saravanan6.kumar6@cognizant.com>
2020-01-02 03:54:22 +0000

    Basic Template

---
+++ app/src/main/java/com/nothingspecial/mvicourse/ui/main/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.nothingspecial.mvicourse.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nothingspecial.mvicourse.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showMainFragment()
+    }
+
+    fun showMainFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(
+                R.id.fragment_container,
+                MainFragment(), "MainFragment"
+            )
+            .commit();
+    }
+}
