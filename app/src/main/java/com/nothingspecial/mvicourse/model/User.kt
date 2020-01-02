044560d68a9ce38505fd3ebc61fd2fb74072c490
154773 <saravanan6.kumar6@cognizant.com>
2020-01-02 03:55:25 +0000

    Basic Model

---
+++ app/src/main/java/com/nothingspecial/mvicourse/model/User.kt
@@ -0,0 +1,20 @@
+package com.nothingspecial.mvicourse.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class User(
+    @Expose
+    @SerializedName("email")
+    val email: String? = null,
+
+    @Expose
+    @SerializedName("username")
+    val userName: String? = null,
+
+    @Expose
+    @SerializedName("image")
+    val image: String? = null
+
+) {
+}
